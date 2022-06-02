cd8d092181946d502431ea52a9e8aa348cd03b85
Kim Ho <Kim Ho@DESKTOP-HKNSKPH>
2022-06-02 09:55:57 +0900

    Badges 2022.06.02 09.55 add Domain,Dao,Service

---
+++ GodLife/src/main/java/com/godlife/io/service/product/ProductDao.java
@@ -0,0 +1,108 @@
+package com.godlife.io.service.product;
+
+import java.util.List;
+
+import com.godlife.io.common.Search;
+import com.godlife.io.service.domain.Badge;
+import com.godlife.io.service.domain.MyBadge;
+import com.godlife.io.service.domain.Product;
+
+
+//==> 회원관리에서 CRUD 추상화/캡슐화한 DAO Interface Definition
+public interface ProductDao {
+	
+///////////////////////////////////////////////////////////////
+	// 신규 쿠폰 상품 등록 UI 요청 
+	public void addProductCouponView(Product product) throws Exception ;	
+	// 신규 쿠폰 상품 등록 완료 요청
+	public void addProductCoupon(Product product) throws Exception ;
+	// 신규 쿠폰 상품 등록 확인 요청
+	public void addProductCouponConfirm(Product product) throws Exception ;
+	// 신규 배지 이미지 등록 완료
+	public void addProductCouponImg(Product product) throws Exception ;
+	
+	// 신규 상품권 상품 등록 UI 요청 
+	public void addProductVoucherView(Product product) throws Exception ;	
+	// 신규 상품권 상품 등록 완료 요청
+	public void addProductVoucher(Product product) throws Exception ;
+	// 신규 상품권 상품 등록 확인 요청
+	public void addProductVoucherConfirm(Product product) throws Exception ;
+	// 신규 상품권 이미지 등록 완료
+	public void addProductVoucherImg(Product product) throws Exception ;
+
+	// 신규 포인트 상품 등록 UI 요청 
+	public void addProductPointView(Product product) throws Exception ;	
+	// 신규 포인트 상품 등록 완료 요청
+	public void addProductPoint(Product product) throws Exception ;
+	// 신규 포인트 상품 등록 확인 요청
+	public void addProductPointConfirm(Product product) throws Exception ;
+	// 신규 포인트 이미지 등록 완료
+	public void addProductPointImg(Product product) throws Exception ;
+	
+///////////////////////////////////////////////////////////////	
+	
+	// 쿠폰 상품 상세 내용 요청 
+	public Product getProductCoupon(int productNo) throws Exception ;
+	// 상품권 상품 상세 내용 요청 
+	public Product getProductVoucher(int productNo) throws Exception ;
+	// 포인트 상품 상세 내용 요청 
+	public Product getProductPoint(int productNo) throws Exception ;
+	
+///////////////////////////////////////////////////////////////	
+	
+	// 쿠폰 상품 전체 목록 UI 요청
+	public List<Product> getProductCouponList(Search search) throws Exception ;
+	// 상품권 상품 전체 목록 UI 요청
+	public List<Product> getProductVoucherList(Search search) throws Exception ;
+	// 포인트 상품 전체 목록 UI 요청
+	public List<Product> getProductPointList(Search search) throws Exception ;
+	
+///////////////////////////////////////////////////////////////
+	
+	// 쿠폰 상품 이미지 수정
+	public void updateProductCouponImg(Product product) throws Exception ;	
+	// 쿠폰 상품 내용 수정 완료 요청
+	public void updateProductCoupon(Product product) throws Exception ;	
+	// 쿠폰 상품 내용 수정 확인 요청
+	public void updateProductCouponConfirm(Product product) throws Exception ;	
+
+	// 상품권 상품 이미지 수정
+	public void updateProductVoucherImg(Product product) throws Exception ;	
+	// 상품권 상품 내용 수정 완료 요청
+	public void updateProductVoucher(Product product) throws Exception ;	
+	// 상품권 상품 내용 수정 확인 요청
+	public void updateProductVoucherConfirm(Product product) throws Exception ;
+
+	// 포인트 상품 이미지 수정
+	public void updateProductPointImg(Product product) throws Exception ;	
+	// 포인트 상품 내용 수정 완료 요청
+	public void updateProductPoint(Product product) throws Exception ;	
+	// 포인트 상품 내용 수정 확인 요청
+	public void updateProductPointConfirm(Product product) throws Exception ;
+
+///////////////////////////////////////////////////////////////
+	
+	// DELETE (기존꺼에 추가 Data)
+	// 쿠폰 상품 삭제 완료 요청
+	public void deleteProductCoupon(Product product) throws Exception ;
+	// 쿠폰 상품 삭제 확인 요청
+	public void deleteProductCouponConfirm(Product product) throws Exception ;	
+
+	// 상품권 상품 삭제 완료 요청
+	public void deleteProductVoucher(Product product) throws Exception ;
+	// 상품권 상품 삭제 확인 요청
+	public void deleteProductVoucherConfirm(Product product) throws Exception ;	
+
+	// 포인트 상품 삭제 완료 요청
+	public void deleteProductPoint(Product product) throws Exception ;
+	// 포인트 상품 삭제 확인 요청
+	public void deleteProductPointConfirm(Product product) throws Exception ;	
+	
+///////////////////////////////////////////////////////////////
+	
+	// 게시판 Page 처리를 위한 전체Row(totalCount)  return (기존 Data)
+	public int getTotalCount(Search search) throws Exception ;
+	
+	
+
+}
